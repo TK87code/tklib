52a5ee8b2c373573085b34b2672499d29d204efe
takuya0311 <t.kimura0311@gmail.com>
2024-12-10 16:41:45 +0900

    tkmt_rand(), tkmt_randf(), tkmt_srand()

--- tk_math.c
+++ tk_math.c
@@ -1,4 +1,6 @@
 #include "tk_math.h"
+#include <time.h> /* time */
+#include <stdlib.h> /* rand() */
 
 int tkmt_clamp(int value_to_clamp, int min, int max)
 {
@@ -18,4 +20,18 @@ float tkmt_clampf(float value_to_clamp, float min, float max)
         return max;
     
     return value_to_clamp;
+}
+
+void tkmt_srand(void)
+{
+    srand((unsigned int)time(NULL));
+}
+
+int tkmt_rand(int min, int max){
+    return min + (int)(rand() * (max - min + 1) / (1 + RAND_MAX));
+}
+
+float tkmt_randf(float min, float max)
+{
+    return ((max - min) * ((float)rand() / RAND_MAX)) + min;
 }
--- tk_math.h
+++ tk_math.h
@@ -20,4 +20,23 @@ int tkmt_clamp(int value_to_clamp, int min, int max);
 */
 float tkmt_clampf(float value_to_clamp, float min, float max);
 
+/**
+* @brief Set random seed with current time
+*/
+void tkmt_srand(void);
+
+/**
+* @brief Return pseudo randomly generated integer.
+ * @param min A minimum value of random value
+* @param max A maximum value of random value
+*/
+int tkmt_rand(int min, int max);
+
+/**
+* @brief Return pseudo randomly generated float.
+ * @param min A minimum value of random value
+* @param max A maximum value of random value
+*/
+float tkmt_randf(float min, float max);
+
 #endif //TK_MATH_H
