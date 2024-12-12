f5c0b95cab73a1bfe148e037157ec989da6d5b57
takuya0311 <t.kimura0311@gmail.com>
2024-12-12 10:27:25 +0900

    deleted bugged functions

--- tk_string.h
+++ tk_string.h
@@ -1,5 +1,3 @@
 
 /* Function prototypes */
 extern char* tkstr_replace(char *src, char *find, char *replace_with);
-extern int tkstr_erase_after_char(char **src, char find);
-extern int tkstr_erase_until_char(char **src, char find);
